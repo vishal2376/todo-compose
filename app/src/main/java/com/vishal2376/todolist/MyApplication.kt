5029d49718513552dd4ea9768696cec287b01a8a
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-25 12:22:45 +0530

    setup hilt

---
+++ app/src/main/java/com/vishal2376/todolist/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.vishal2376.todolist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/vishal2376/todolist/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.vishal2376.todolist.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+	
+}
